243def58c2abcfcae34697cf5c41fcb4b3cea66d
Riam Djadri <rdjadri@regmail.west.com>
2021-01-26 08:11:22 -0500

    Implement async await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,41 +1,26 @@
 const express = require('express');
 const fs = require('fs');
+const fsPromises = require("fs").promises;
 const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
 router.route('/')
-  .get(function (req, res) {
-
-    getClothingData()
-      .then((data) => {
-        res.send(data);
-        console.log("Sending data to client.");
-      })
-      .catch((error) => {
-        res.status(500).send(error);
-        console.log(error);
-      })
-      .finally(() => {
-        console.log("Promise work is all done.");
-      });
-
-    console.log("Doing other work.");
+  .get(async function (req, res) {
+    try {
+      let clothingData = await getClothingData();
+      console.log("Sending data to client");
+      res.send(clothingData);
+    } catch (error) {
+      res.status(500).send(error);
+      console.log(error);
+    }
   });
 
-function getClothingData() {
-  return new Promise((resolve, reject) => {
-    console.log("Reading from disk");
-    fs.readFile(datafile, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      else {
-        let clothingData = JSON.parse(data);
-        resolve(clothingData);
-      };
-    });
-  });
+async function getClothingData() {
+  console.log("Reading from disk");
+  let fileData = await fsPromises.readFile(datafile, "utf8");
+  return JSON.parse(fileData);
 }
 
 module.exports = router;
